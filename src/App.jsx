8b531bfcb33268326b59532f163b5c637bbd0185
marin0suke <monk.marina@gmail.com>
2025-01-31 14:29:59 +1000

    Add styling.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import GeneralInfo from './components/GeneralInfo'
 import Education from './components/Education' 
 import Experience from './components/Experience'
 import CVDisplay from './components/CVDisplay'
+import './styles/App.css'
 
 function App() {
   const [formData, setFormData] = useState({
@@ -12,28 +13,25 @@ function App() {
     experience: [{company: '', position: '', responsibilities: '', startDate: '', endDate: ''}],
   });
 
-  const updateGeneralInfo = (newData) => {
-    setFormData((prev) => ({ ...prev, generalInfo: newData }));
-  };
-
-  const updateEducation = (newData) => {
-    setFormData((prev) => ({ ...prev, education: newData }));
-  };
-
-  const updateExperience = (newData) => {
-    setFormData((prev) => ({ ...prev, experience: newData }));
-  };
-
+  const updateFormData = (section, newData) => {
+    setFormData((prev) => ({ ...prev, [section]: newData}));
+  }
 
 
   return (
     <>
-      <h1>CV Generator</h1>
-      <GeneralInfo data={formData.generalInfo} onUpdate={updateGeneralInfo} />
-      <Education data={formData.education} onUpdate={updateEducation} />
-      <Experience data={formData.experience} onUpdate={updateExperience} />
+    <h1>CV Generator</h1>
+    <div className="container">  
+      <div className="form-container">
+        <GeneralInfo data={formData.generalInfo} onUpdate={(newData) => updateFormData('generalInfo', newData)} />
+        <Education data={formData.education} onUpdate={(newData) => updateFormData('education', newData)} />
+        <div className="experience-container"> 
+          <Experience data={formData.experience} onUpdate={(newData) => updateFormData('experience', newData)} />
+        </div>
+      </div>
       <CVDisplay formData={formData} />
-    </>
+    </div>
+  </>
   );
 }
 
